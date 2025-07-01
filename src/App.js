3677230c12f84982bcbca60b182f1f8acf1b577e
Shrihari <140818388+shrihariss29@users.noreply.github.com>
2025-07-01 11:22:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
       <div className="loading-content">
           <img src={sc} alt="Sand clock" className="rotating-image"/>
         <h1>Vinterbash 2025</h1>
-        <p>Website will be live from <strong>30th June 2025</strong></p>
+        <p>Website will be live from <strong>1st July 2025</strong></p>
+    <p>Registration starts <strong>2nd July 2025 11 am</strong></p>
       </div>
     </div>      
   );
